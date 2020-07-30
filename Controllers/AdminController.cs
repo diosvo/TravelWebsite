6a149a526f72ec2701bc6b8d87ffbc5bef788649
Dios Vo <51939323+diosvo@users.noreply.github.com>
2020-07-30 17:40:17 +0700

    add packages

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using TravelWebsite.Models;
 
@@ -7,7 +9,8 @@ namespace TravelWebsite.Controllers
     public class AdminController : Controller
     {
         cs c = new cs();
-        // GET: Admin
+
+        /* Admin */
         public ActionResult Index()
         {
             return View();
@@ -19,7 +22,7 @@ namespace TravelWebsite.Controllers
             var x = c.Logins.FirstOrDefault(a => a.Email.Equals(login.Email) && a.Password.Equals(login.Password));
             if (x != null)
             {
-                return View("AdminDashboard");
+                return View("AddPackages");
             }
             else
             {
@@ -28,6 +31,7 @@ namespace TravelWebsite.Controllers
             }
         }
 
+        /* Sign Up - Registration (Reg) */
         [HttpGet]
         public ActionResult Reg()
         {
@@ -40,6 +44,47 @@ namespace TravelWebsite.Controllers
             c.Logins.Add(login);
             c.SaveChanges();
             return View("Index");
+        }
+
+        /* Packages */
+        [HttpGet]
+        public ActionResult Packages()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPackages(Package p, string Offers, HttpPostedFileBase image)
+        {
+            string picture = Path.Combine(Server.MapPath("~/Images"), image.FileName);
+            image.SaveAs(picture);
+            var pkg = c.Packages.Where(e => e.Offer == Offers);
+            p.Image = picture;
+            p.Offer = Offers;
+            c.Packages.Add(p);
+            c.SaveChanges();
+            ViewBag.pk = "Package added successfully !";
+            return View();
+        }
+
+        /* Services */
+        [HttpGet]
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddServices(Services services, string Services, HttpPostedFileBase image)
+        {
+            string picture = Path.Combine(Server.MapPath("~/Images"), image.FileName);
+            image.SaveAs(picture);
+            services.Image = picture;
+            services.Service = Services;
+            c.Services.Add(services);
+            c.SaveChanges();
+            ViewBag.sv = "Servicess added successfully";
+            return View();
         }
     }
 }
